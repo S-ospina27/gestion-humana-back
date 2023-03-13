e5ddb26961ddc539b048790ad5247fab48a1eb30
Sergio León <sergioleon4004@hotmail.com>
2023-03-13 12:42:17 -0500

    first commit

---
+++ routes/web.php
@@ -0,0 +1,21 @@
+<?php
+
+use App\Http\Controllers\Auth\LoginController;
+use App\Http\Controllers\HomeController;
+use LionRoute\Route;
+
+/**
+ * ------------------------------------------------------------------------------
+ * Web Routes
+ * ------------------------------------------------------------------------------
+ * Here is where you can register web routes for your application
+ * ------------------------------------------------------------------------------
+ **/
+
+Route::any('/', [HomeController::class, 'index']);
+
+Route::prefix('api', function() {
+    Route::prefix('auth', function() {
+        Route::post('login', [LoginController::class, 'auth']);
+    });
+});
---
+++ storage/framework/helpers.php
@@ -0,0 +1,82 @@
+<?php
+
+/**
+ * ------------------------------------------------------------------------------
+ * framework level predefined constants
+ * ------------------------------------------------------------------------------
+ **/
+
+define('client', new GuzzleHttp\Client());
+define('request', LionRequest\Request::getInstance()->capture());
+define('response', LionRequest\Response::getInstance());
+define('json', LionRequest\Json::getInstance());
+define('env', (object) $_ENV);
+
+/**
+ * ------------------------------------------------------------------------------
+ * Function to make HTTP requests with guzzlehttp
+ * ------------------------------------------------------------------------------
+ **/
+
+if (!function_exists('fetch')) {
+    function fetch(string $method, string $uri, array $options = []): array {
+        return json->decode(client->request($method, $uri, $options)->getBody());
+    }
+}
+
+/**
+ * ------------------------------------------------------------------------------
+ * Function to get the path of the storage directory
+ * ------------------------------------------------------------------------------
+ **/
+
+if (!function_exists('storage_path')) {
+    function storage_path(string $path = ""): string {
+        return "../storage/{$path}";
+    }
+}
+
+/**
+ * ------------------------------------------------------------------------------
+ * Function to display a response and end the execution of processes
+ * ------------------------------------------------------------------------------
+ **/
+
+if (!function_exists('finish')) {
+    function finish(mixed $response): void {
+        response->finish($response);
+    }
+}
+
+/**
+ * ------------------------------------------------------------------------------
+ * Function to perform a var_dump
+ * ------------------------------------------------------------------------------
+ **/
+
+if (!function_exists('vd')) {
+    function vd(mixed $response): void {
+        var_dump($response);
+    }
+}
+
+/**
+ * ------------------------------------------------------------------------------
+ * Function to add a line to the log file
+ * ------------------------------------------------------------------------------
+ **/
+
+if (!function_exists('logger')) {
+    function logger(string $str, string $log_type = 'info', array $data = [], bool $index = true): void {
+        $file_name = "lion-" . Carbon\Carbon::now()->format("Y-m-d") . ".log";
+        $path = !$index ? "storage/logs/" : storage_path("logs/");
+        LionFiles\Store::folder($path);
+
+        (new Monolog\Logger('log'))->pushHandler(
+            new Monolog\Handler\StreamHandler(
+                ($path . $file_name),
+                Monolog\Level::Debug
+            )
+        )->$log_type($str, $data);
+    }
+}
